4822ac75506c227f46a900693bad2f4932bdae03
Ajanaku Oluwatosin Ezekiel <ajanaku.similoluwa@gmail.com>
2024-01-12 11:57:05 +0100

    javascript source code for web socket connection

---
+++ javascript_files/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+
+const WS_PORT  = 8888;
+const HTTP_PORT = 8000;
+
+const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
+
+let connectedClients = [];
+wsServer.on('connection', (ws, req)=>{
+    console.log('Connected');
+    connectedClients.push(ws);
+
+    ws.on('message', data => {
+        connectedClients.forEach((ws,i)=>{
+            if(ws.readyState === ws.OPEN){
+                ws.send(data);
+            }else{
+                connectedClients.splice(i ,1);
+            }
+        })
+    });
+});
